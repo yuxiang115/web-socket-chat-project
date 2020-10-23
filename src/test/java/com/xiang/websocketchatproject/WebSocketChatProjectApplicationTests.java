fb55bd8647ae288168d9cc1d2b59bee919338249
Xiang Yu <yuxiang115@126.com>
2020-10-23 15:47:29 -0700

    first

---
+++ src/test/java/com/xiang/websocketchatproject/WebSocketChatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiang.websocketchatproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketChatProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
